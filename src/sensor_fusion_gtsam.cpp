9f12d62440e3590f642ae9f5f3aff0d3a730e293
ZoeLiLi <yuerlei14@163.com>
2019-03-12 03:20:59 -0400

    ID:FG-6 first try,fail

--- src/sensor_fusion_gtsam.cpp
+++ src/sensor_fusion_gtsam.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 #include <Eigen/Core>
 
 #include <sophus/se3.h>
@@ -34,7 +35,16 @@ using Sophus::SO3;
  * sphere.g2o 是人工生成的一个 Pose graph，我们来优化它。
  * 与 g2o 相似，在 gtsam 中添加的是因子，相当于误差
  * **********************************************/
- typedef struct
+#define Re 6378137
+#define e2 0.00669437999013
+#define we 7.2921151467e-5
+#define PI (4*atan(1.0))
+#define DEG2RAD (PI/180)
+#define RAD2DEG (180/PI)
+#define t_shift 1543221000
+#define DPH_2_RPS (DEG2RAD/3600.0)
+#define MG_2_MPS2 (1e-3*9.7803267714)
+typedef struct
  {
 	 double t;
 	 double dt;
@@ -47,6 +57,8 @@ using Sophus::SO3;
  }IMUData;
  typedef struct{
 	 double t;
+	 double Rm;
+	 double Rn;
 	 gtsam::Vector3 pos;
  }GPSData;
  typedef struct
@@ -128,78 +140,41 @@ using Sophus::SO3;
    PreintegratedCombinedMeasurements* preintegrated;
  };
  vector<IMUData> imuData;
+ vector<GPSData> gpsData;
 void findIMUIndex(double tEnd,double tStart,int& startIndex,vector<int>& imuIndex);
+bool loadDRLog(string filePath, int& cntIMU, int& cntGPS);
+double CalculateRm(double lat);
+double CalculateRn(double lat);
 int main ( )
 {
 	int cntVertex = 0,cntEdge = 0;
 	int cntIMU = 0,cntGPS = 0;
 	int startIndex = 0;
-    ifstream imuifs("../../data/KittiEquivBiasedImu.txt");
-    ifstream gpsifs("../../data/KittiGps_converted.txt");
+	string DRlogPath("../../data/GPS_IMU_20181126.txt");
     ofstream resultofs("../../data/results.txt");
-    if(!imuifs.is_open())
-    {
-      cout<<"open imu data file failed!"<<endl;
-    }
-    if(!gpsifs.is_open())
-    {
-    	cout<<"open gps data file failed!"<<endl;
-    }
+
     if(!resultofs.is_open())
      {
      	cout<<"open result data file failed!"<<endl;
      }
 
-    vector<GPSData> gpsData;
-    while(!imuifs.eof())
-    {
-    	IMUData data;
-    	string str;
-    	getline(imuifs,str);
-    	vector<string> vTemp;
-    	boost::split(vTemp,str,boost::is_any_of(" "));
-    	if((vTemp[0] != "Time" && vTemp[0] != "") && vTemp.size() == 8)
-    	{
-    		data.t = atof(vTemp[0].c_str());
-    		data.dt = atof(vTemp[1].c_str());
-    		data.ax = atof(vTemp[2].c_str());
-    		data.ay = atof(vTemp[3].c_str());
-    		data.az = atof(vTemp[4].c_str());
-    		data.gx = atof(vTemp[5].c_str());
-    		data.gy = atof(vTemp[6].c_str());
-    		data.gz = atof(vTemp[7].c_str());
-    		imuData.push_back(data);
-    		cntIMU++;
-    	}
-    }
-    while(!gpsifs.eof())
-    {
-    	GPSData data;
-    	string str;
-    	getline(gpsifs,str);
-    	vector<string> vTemp;
-    	boost::split(vTemp,str,boost::is_any_of(","));
-    	if(vTemp[0]!="Time" && vTemp[0] != "")
-    	{
-    		data.t = atof(vTemp[0].c_str());
-    		data.pos[0] = atof(vTemp[1].c_str());
-    		data.pos[1] = atof(vTemp[2].c_str());
-    		data.pos[2] = atof(vTemp[3].c_str());
-    		gpsData.push_back(data);
-    		cntGPS++;
-    	}
-    }
+    resultofs.setf(ios::fixed,ios::floatfield);
+    resultofs.precision(8);
+    bool loadDataFlag = loadDRLog(DRlogPath,cntIMU,cntGPS);
+    cout<<"IMU count:" <<cntIMU<<endl;
+    cout<<"GPS count:" <<cntGPS<<endl;
 
     gtsam::Vector3 g(0.0,0.0,-9.8);
     gtsam::Vector3 w_coriolis(0.0,0.0,0.0);
     gtsam::Vector3 zero(0.0,0.0,0.0);
     gtsam::Matrix3 unit = gtsam::Matrix3::Identity();
-    int firstGPSPose = 1;
     
-    gtsam::Rot3 R;
-    gtsam::Point3 t(gpsData[0].pos);
+    gtsam::Rot3 R(-0.0069812603,0.0,0.0,0.9999756307);
+    gtsam::Point3 t(gpsData[16].pos);
 
     // bias noise model
+    gtsam::Vector3 gyroBias(-500*DPH_2_RPS,500*DPH_2_RPS,-1600*DPH_2_RPS);
+    gtsam::Vector3 accBias(0,0,40*MG_2_MPS2);
     gtsam::Vector biasSigma(6);
     biasSigma<< gtsam::Vector3::Constant(0.1),gtsam::Vector3::Constant(5.0e-5);
 
@@ -209,8 +184,8 @@ int main ( )
     gtsam::SharedNoiseModel noiseModelGPS = gtsam::noiseModel::Diagonal::Precisions(gpsSigma, 1);
 
     gtsam::Pose3 currentPos(gtsam::Pose3(R,t));
-    gtsam::Vector3 currentVel(0.0,0.0,0.0);
-    gtsam::imuBias::ConstantBias currentBias(zero,zero);
+    gtsam::Vector3 currentVel(0.081158,-5.8121,0.0);
+    gtsam::imuBias::ConstantBias currentBias(accBias,gyroBias);
     
     gtsam::SharedNoiseModel initSigmaP =gtsam::noiseModel::Diagonal::Sigmas(
     		(gtsam::Vector(6) <<gtsam::Vector3::Constant(0.1),gtsam::Vector3::Constant(0.1)).finished());
@@ -232,29 +207,25 @@ int main ( )
 
 
     gtsam::NonlinearFactorGraph newFactors;
-
     gtsam::Values newValues;
-    gtsam::NonlinearFactorGraph newFactors0;
-
-    gtsam::Values newValues0;
 
     IMUHelper imu;
 
-    int imuIndex = 2,gpsIndex=1;
-    double currentT,previousT;
-
+    int imuIndex = 11600,gpsIndex=16;
+    double currentT;
     int vertexIndex = 0;
     while(gpsData[gpsIndex].t < imuData[imuIndex].t)
     {
     	gpsIndex ++;
     }
     bool initialed = false;
-    bool gpsUpdate = false;
     int count = 0;
-    int count0 = 0;
+
     vector<Graph_Info> graphFactorVec;
     gtsam::NonlinearFactorGraph factorVec;
-    int windowLength = 70; //10s
+    int windowLength = 100; //10s
+    double Rm,Rn;
+    double lat,lon ,h;
     while(imuIndex<cntIMU)
     {
     	currentT = imuData[imuIndex].t;
@@ -287,9 +258,15 @@ int main ( )
     		    graphFactor.bias = currentBias;
     		    graphFactorVec.push_back(graphFactor);
 
+    		    Rm = gpsData[gpsIndex].Rm;
+				Rn = gpsData[gpsIndex].Rn;
+
+				h = currentPos.z();
+				lat = currentPos.y()/(Rm + h);
+				lon = currentPos.x()/((Rn+h)*cos(lat));
+    		    resultofs<<currentT<<" "<< lat*RAD2DEG<<" "<<lon*RAD2DEG<<" "<<h<<" "<<currentVel.transpose()<<endl;
 
 
-    			previousT = currentT;
     			initialed = true;
     			gpsIndex ++;
     			vertexIndex ++;
@@ -302,14 +279,21 @@ int main ( )
     	}
     	else
     	{
-    		if(currentT - previousT > 0)
-    			{
-    		    	gtsam::Vector3 accMeas(imuData[imuIndex].ax,imuData[imuIndex].ay,imuData[imuIndex].az);
-    		    	gtsam::Vector3 gyroMeas(imuData[imuIndex].gx,imuData[imuIndex].gy,imuData[imuIndex].gz);
-    		    	double dt = imuData[imuIndex].dt;
-    		    	imu.preintegrated->integrateMeasurement(accMeas,gyroMeas,dt);
-    		    	count +=1;
-    			}
+    		cout<<"Vertex: "<<vertexIndex<<endl;
+			gtsam::Vector3 accMeas(imuData[imuIndex].ax,imuData[imuIndex].ay,imuData[imuIndex].az);
+			gtsam::Vector3 gyroMeas(imuData[imuIndex].gx,imuData[imuIndex].gy,imuData[imuIndex].gz);
+			double dt = imuData[imuIndex].dt;
+
+			if(dt > 2*0.02)
+			{
+				dt = 0.02;
+			}
+			if(dt != 0)
+			{
+				imu.preintegrated->integrateMeasurement(accMeas,gyroMeas,dt);
+			}
+			count +=1;
+
     		if(fabs(gpsData[gpsIndex].t-currentT) < 0.5*imuData[imuIndex].dt)
     		{
     			gtsam::Pose3 gpsPos(gtsam::Pose3(currentPos.rotation(),gpsData[gpsIndex].pos));
@@ -319,14 +303,12 @@ int main ( )
     			factorVec.push_back(gpsFactor);
     			cout<< "GPS Index: "<<gpsIndex<<endl;
     			gpsIndex ++;
-    			gpsUpdate = true;
     		}
-    		else{
-    			gpsUpdate = false;
-    		}
+
     			//add Vertex;
-    		if(count >= 10) //25Hz
+    		if(count >= 50) //1Hz
     		{
+    			cout<<"vertex: "<<vertexIndex<<endl;
     			// add imu factor
     			gtsam::NonlinearFactor::shared_ptr imuFactor(new
     					gtsam::CombinedImuFactor(X(vertexIndex-1),V(vertexIndex-1),X(vertexIndex),V(vertexIndex),B(vertexIndex-1),B(vertexIndex),*imu.preintegrated));
@@ -338,6 +320,7 @@ int main ( )
     			gtsam::NonlinearFactor::shared_ptr betweenFactorConstantBias(new gtsam::BetweenFactor<gtsam::imuBias::ConstantBias>
     			    	(B(vertexIndex-1),B(vertexIndex),gtsam::imuBias::ConstantBias(zero,zero),
     			    	gtsam::noiseModel::Diagonal::Sigmas(imuBiasSigmas)));
+
 
     			graphFactorVec[graphFactorVec.size()-1].factorVec.push_back(imuFactor);
     			graphFactorVec[graphFactorVec.size()-1].factorVec.push_back(betweenFactorConstantBias);
@@ -351,90 +334,102 @@ int main ( )
     			int length = graphFactorVec.size();
     			vector<Graph_Info>::iterator it;
     			FastVector<gtsam::NonlinearFactor::shared_ptr>::iterator fit;
-    			if(length <= windowLength)
-    			{
-    				for(int i=0;i<=vertexIndex;i++)
+
+    			if(gpsIndex >= 5)
+				{
+					if(length <= windowLength)
 					{
-						newValues.insert(X(graphFactorVec[i].vertexIndex),graphFactorVec[i].pos);
-						newValues.insert(V(graphFactorVec[i].vertexIndex),graphFactorVec[i].vel);
-						newValues.insert(B(graphFactorVec[i].vertexIndex),graphFactorVec[i].bias);
-						if(graphFactorVec[i].factorVec.size()!=0)
+						for(int i=0;i<=vertexIndex;i++)
 						{
-							for(fit = graphFactorVec[i].factorVec.begin();fit != graphFactorVec[i].factorVec.end();fit++)
+							newValues.insert(X(graphFactorVec[i].vertexIndex),graphFactorVec[i].pos);
+							newValues.insert(V(graphFactorVec[i].vertexIndex),graphFactorVec[i].vel);
+							newValues.insert(B(graphFactorVec[i].vertexIndex),graphFactorVec[i].bias);
+							if(graphFactorVec[i].factorVec.size()!=0)
 							{
-								newFactors.push_back(*fit);
+								for(fit = graphFactorVec[i].factorVec.begin();fit != graphFactorVec[i].factorVec.end();fit++)
+								{
+									newFactors.push_back(*fit);
+								}
 							}
 						}
 					}
-    			}
-    			else
-    			{
-
-    				it = graphFactorVec.end();
-    				it -= windowLength;
-    				newValues.insert(X((*it).vertexIndex),(*it).pos);
-					newValues.insert(V((*it).vertexIndex),(*it).vel);
-					newValues.insert(B((*it).vertexIndex),(*it).bias);
-					cout<< (*it).vertexIndex<<endl;
-
-					newFactors.add(gtsam::PriorFactor<gtsam::Pose3>(X((*it).vertexIndex+1),(*it).pos,initSigmaP));
-					newFactors.add(gtsam::PriorFactor<gtsam::imuBias::ConstantBias>(B((*it).vertexIndex+1),currentBias,initSigmaB));
-					newFactors.add(gtsam::PriorFactor<gtsam::Vector3>(V((*it).vertexIndex+1),currentVel,initSigmaV));
-
-					if((*it).factorVec.size()!=0)
+					else
 					{
-						for(fit = (*it).factorVec.begin();fit != (*it).factorVec.end();fit++)
-						{
-							if(typeid(*(*fit)) != typeid(gtsam::PriorFactor<gtsam::Pose3>) &&
-								typeid(*(*fit)) != typeid(gtsam::PriorFactor<gtsam::imuBias::ConstantBias>) &&
-								typeid(*(*fit)) != typeid(gtsam::PriorFactor<gtsam::Vector3>))
-							{
-								newFactors.push_back(*fit);
-							}
-						}
-					}
-					for(++it; it !=graphFactorVec.end() ;it++)
-					{
+						it = graphFactorVec.end();
+						it -= windowLength;
 						newValues.insert(X((*it).vertexIndex),(*it).pos);
 						newValues.insert(V((*it).vertexIndex),(*it).vel);
 						newValues.insert(B((*it).vertexIndex),(*it).bias);
+
+						newFactors.add(gtsam::PriorFactor<gtsam::Pose3>(X((*it).vertexIndex+1),(*it).pos,initSigmaP));
+						newFactors.add(gtsam::PriorFactor<gtsam::imuBias::ConstantBias>(B((*it).vertexIndex+1),currentBias,initSigmaB));
+						newFactors.add(gtsam::PriorFactor<gtsam::Vector3>(V((*it).vertexIndex+1),currentVel,initSigmaV));
+
 						if((*it).factorVec.size()!=0)
 						{
 							for(fit = (*it).factorVec.begin();fit != (*it).factorVec.end();fit++)
 							{
-								newFactors.push_back(*fit);
+								if(typeid(*(*fit)) != typeid(gtsam::PriorFactor<gtsam::Pose3>) &&
+									typeid(*(*fit)) != typeid(gtsam::PriorFactor<gtsam::imuBias::ConstantBias>) &&
+									typeid(*(*fit)) != typeid(gtsam::PriorFactor<gtsam::Vector3>))
+								{
+									newFactors.push_back(*fit);
+								}
 							}
 						}
-					 }
-    			}
+						for(++it; it !=graphFactorVec.end() ;it++)
+						{
+							newValues.insert(X((*it).vertexIndex),(*it).pos);
+							newValues.insert(V((*it).vertexIndex),(*it).vel);
+							newValues.insert(B((*it).vertexIndex),(*it).bias);
+							if((*it).factorVec.size()!=0)
+							{
+								for(fit = (*it).factorVec.begin();fit != (*it).factorVec.end();fit++)
+								{
+									newFactors.push_back(*fit);
+								}
+							}
+						 }
+					}
 
-    			gtsam::ISAM2 isam(isamParams);
-    			gtsam::ISAM2Result isam2Result = isam.update(newFactors, newValues);
-    			gtsam::Values result = isam.calculateEstimate();
+					if(vertexIndex >= 1)
+					{
+						newValues.print();
+//						newFactors.print();
+					}
 
-    			currentPos = result.at<gtsam::Pose3>(X(vertexIndex));
-    			currentVel = result.at<gtsam::Vector3>(V(vertexIndex));
-    			currentBias = result.at<gtsam::imuBias::ConstantBias>(B(vertexIndex));
+					gtsam::ISAM2 isam(isamParams);
+					gtsam::ISAM2Result isam2Result = isam.update(newFactors, newValues);
+					gtsam::Values result = isam.calculateEstimate();
 
-    			graphFactorVec[graphFactorVec.size()-1].pos = currentPos;
-    			graphFactorVec[graphFactorVec.size()-1].vel = currentVel;
-    			graphFactorVec[graphFactorVec.size()-1].bias = currentBias;
+					currentPos = result.at<gtsam::Pose3>(X(vertexIndex));
+					currentVel = result.at<gtsam::Vector3>(V(vertexIndex));
+					currentBias = result.at<gtsam::imuBias::ConstantBias>(B(vertexIndex));
 
-    			resultofs<<currentT<<" "<< currentPos.x()<<" "<<currentPos.y()<<" "<<currentPos.z()<<" "<<currentVel.transpose()<<endl;
+					graphFactorVec[graphFactorVec.size()-1].pos = currentPos;
+					graphFactorVec[graphFactorVec.size()-1].vel = currentVel;
+					graphFactorVec[graphFactorVec.size()-1].bias = currentBias;
 
-    			newFactors.resize(0);
-    			newValues.clear();
+
+					h = currentPos.z();
+					lat = currentPos.y()/(Rm + h);
+					lon = currentPos.x()/((Rn+h)*cos(lat));
+					Rm = CalculateRm(lat);
+					Rn = CalculateRn(lat);
+					resultofs<<currentT<<" "<< lat*RAD2DEG<<" "<<lon*RAD2DEG<<" "<<h<<" "<<currentVel.transpose()<<endl;
+					newFactors.resize(0);
+					newValues.clear();
+				}
+
     			imu.preintegrated->resetIntegrationAndSetBias(currentBias);
     			vertexIndex ++;
     			factorVec.resize(0);
     			count = 0;
     		}
-    		previousT = currentT;
-    		imuIndex++;
     	}
+    	imuIndex++;
     }
-    imuifs.close();
-    gpsifs.close();
+    cout<<"end"<<endl;
     resultofs.close();
 	return 0;
 }
@@ -453,4 +448,112 @@ void findIMUIndex(double tEnd,double tStart,int& startIndex,vector<int>& imuInde
 		startIndex ++;
 	}
 }
+bool loadDRLog(string filePath, int& cntIMU, int& cntGPS)
+{
+	ifstream ifs(filePath.c_str());
+	if(!ifs.is_open())
+	{
+		cout<<"failed to open data file"<<endl;
+		return false;
+	}
+	double Rm,Rn;
+	double lat,lon,h;
+	unsigned long long t;
+	cntIMU = 0;
+	cntGPS = 0;
+	double previousT = 0.0;
+
+	 ofstream dataOut("../../data/out.txt");
+	if(!dataOut.is_open())
+	 {
+		cout<<"open dataOut file failed!"<<endl;
+	 }
+	IMUData imu;
+	while(!ifs.eof())
+	{
+		string strLine;
+		string gyroLine;
+		string accLine;
+		vector<string> vTemp;
+		getline(ifs,strLine);
+		boost::split(vTemp,strLine,boost::is_any_of(","));
+		if(vTemp[2] == "GYRODATA")
+		{
+			t = atoll(vTemp[3].c_str());
+			imu.t  = t*0.001 - t_shift;
+			imu.gx = atof(vTemp[4].c_str());
+			imu.gy = atof(vTemp[5].c_str());
+			imu.gz = atof(vTemp[6].c_str());
+			dataOut<<strLine;
+		}
+		if(vTemp[2] == "ACCDATA")
+		{
+			t = atoll(vTemp[3].c_str());
+			if((t*0.001-t_shift) == imu.t)
+			{
+				imu.ax = atof(vTemp[4].c_str());
+				imu.ay = atof(vTemp[5].c_str());
+				imu.az = atof(vTemp[6].c_str());
+				if(previousT > 0.0001)
+				{
+					imu.dt = imu.t - previousT;
+				}
+				else
+				{
+					imu.dt = 0.02;
+				}
+				dataOut<<strLine;
+				previousT = imu.t;
+				cntIMU ++;
+				imuData.push_back(imu);
+			}
+		}
+
+		if(vTemp[2] == "GPSDATA")
+		{
+			GPSData gps;
+			t = atoll(vTemp[3].c_str());
+			lat = atof(vTemp[5].c_str())*DEG2RAD;
+			lon = atof(vTemp[4].c_str())*DEG2RAD;
+			h = atof(vTemp[6].c_str());
+			Rm = CalculateRm(lat);
+			Rn = CalculateRn(lat);
+			gps.t = t*0.001-t_shift;
+			gps.pos<< lon*(Rn+h)*cos(lat),lat*(Rm+h),h;
+			gps.Rm = Rm;
+			gps.Rn = Rn;
+			gpsData.push_back(gps);
+			cntGPS ++;
+			dataOut<<strLine;
+			//cout<<gps.t<<" "<<gps.pos[0]/(Rm+h)*RAD2DEG<<" "<<gps.pos[1]/((Rn+h)*cos(lat))*RAD2DEG<<" "<<gps.pos[2]<<endl;
+		}
+	}
+	dataOut.close();
+	if(cntIMU == 0 && cntGPS == 0 )
+	{
+		cout<<"No IMU and GPS data extracted."<<endl;
+		return false;
+	}
+	else
+	{
+		cout<<"IMU count:" <<cntIMU<<endl;
+		cout<<"GPS count:" <<cntGPS<<endl;
+	}
+	return true;
+}
+double CalculateRm(double lat)
+{
+	double Rm = 0.0;
+	double s2 = sin(lat)*sin(lat);
+	Rm = Re*(1-e2)/pow(1-e2*s2,1.5);
+	return Rm;
+}
+double CalculateRn(double lat)
+{
+	double Rn = 0.0;
+	double s2 = sin(lat)*sin(lat);
+	Rn = Re/sqrt(1-e2*s2);
+	return Rn;
+}
+
 
