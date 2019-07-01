4a8a33307ed3a60fe88465f5d8246696a378b9eb
qlli <qlli@telenav.cn>
2019-07-01 05:56:54 -0400

    ID: FG-17 add odometry velocity into the frame

--- src/odometry_class.cpp
+++ src/odometry_class.cpp
@@ -5,10 +5,10 @@ OdometryPara::OdometryPara(boost::shared_ptr<SensorFactors> sensor_factor)
 : sensor_factors_(sensor_factor)
 , initialed_(false)
 , speed_(-1.0)
-, velocity_sigma_(0.1,1.5,0.1)
+, velocity_sigma_(10,15,10)
 {
-//	velocity_noise_model_ = gtsam::noiseModel::Diagonal::Sigmas(velocity_sigma_);
-	velocity_noise_model_ = gtsam::noiseModel::Isotropic::Sigma(1,1.5);
+	velocity_noise_model_ = gtsam::noiseModel::Diagonal::Sigmas(velocity_sigma_);
+	//velocity_noise_model_ = gtsam::noiseModel::Isotropic::Sigma(1,1.5);
 	//velocity_thread_ = new boost::thread(boost::BOOST_BIND(&OdometryPara::GenerateVelocityFactor,this));
 }
 OdometryPara::~OdometryPara()
@@ -39,12 +39,11 @@ void OdometryPara::GenerateVelocityFactor()
 			{
 
 				gtsam::Point3 velocity(0.0,vehicle_data_.speed,0.0);
-//				gtsam::NonlinearFactor::shared_ptr velocity_factor(new VelocityFactor1(
-//						V(sensor_factors_->current_factor_graph_.value_index),velocity,
-//						sensor_factors_->current_factor_graph_.pose.rotation(),velocity_noise_model_));
+				gtsam::NonlinearFactor::shared_ptr velocity_factor(new VelocityFactor2(X(sensor_factors_->current_factor_graph_.value_index),
+						V(sensor_factors_->current_factor_graph_.value_index),velocity,velocity_noise_model_));
 
-				gtsam::NonlinearFactor::shared_ptr velocity_factor(new VelocityFactor1(
-										V(sensor_factors_->current_factor_graph_.value_index),vehicle_data_.speed,velocity_noise_model_));
+//				gtsam::NonlinearFactor::shared_ptr velocity_factor(new VelocityFactor1(
+//										V(sensor_factors_->current_factor_graph_.value_index),vehicle_data_.speed,velocity_noise_model_));
 				sensor_factors_->current_factor_graph_.factors.push_back(velocity_factor);
 
 			}
